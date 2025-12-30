1e516f96063282684f57cbfb968bf7e1b2b472e1
tanzeelalam <tanzeelalam@gmail.com>
2025-12-30 17:19:33 +0530

    Update orbitbuild.groovy

--- src/org/mcafee/orbitbuild.groovy
+++ src/org/mcafee/orbitbuild.groovy
@@ -16,7 +16,13 @@ import com.mcafee.orbit.Credentials.CredentialStore
 
 @Field stashNumber = 0
 @Field stashList = []
-@Field buildInfo = Artifactory.newBuildInfo()
+// @Field buildInfo = Artifactory.newBuildInfo()
+
+// Add constructor to accept script context
+orbitbuild(script) {
+    this.steps = script
+    this.buildInfo = script.Artifactory.newBuildInfo()  // ✅ Initialize here with script context
+}
 
 /**
  * Check out a GIT repository to the workspace.
